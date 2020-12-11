0c408fa31089388b7fb0150e98f4ca750f49afa4
f0xeri <yaroslav-studio@list.ru>
2020-12-11 20:28:14 +0400

    Текущее состояние: есть карта и точки

---
+++ IITIK_HACK_map/ClientApp/src/components/AddressesView.js
@@ -0,0 +1,77 @@
+﻿import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './AddressesView.css'
+
+export const AddressesView = ({coords, setCoord, routeCoords, inputType, setInputType}) => {
+  
+  const [input, setInput] = useState('');
+
+  function onInputChange(e) {
+    e.preventDefault();
+    //setInput(e.target.value.replace(/\s+/g,' '));
+    setInput(e.target.value);
+  }
+  
+  // временно убрал редактирование готовых точек
+  const onCoordChange = (e, coord)  => {
+    /*coord.address = e.target.value.replace(/\s+/g,' ');
+    let coordsTemp = [...coords];
+    let routeCoordsTemp = [...routeCoords.current];
+    coordsTemp[coord.index] = coord;
+
+    routeCoords.current[coord.index] = e.target.value.replace(/\s+/g,' ');
+    console.log(routeCoords);
+    setCoord(coordsTemp);*/
+  }
+  
+  const deleteCoord = coord => {
+    setCoord(coords => coords.filter((e) => e.index !== coord.index));
+    routeCoords.current.splice(coord.index, 1); 
+  }
+  
+  const setCoordsButtonOnClick = e => {
+    e.preventDefault();
+    routeCoords.current = [];
+    routeCoords.current = input.split(";");
+    for (let i = 0; i < routeCoords.current.length; i++) {
+      routeCoords.current[i] = routeCoords.current[i].replaceAll(",", ".").split(" ");
+    }
+    setCoord([]);
+    for (let x in routeCoords.current) {
+      setCoord(coords =>
+        [...coords, {index: coords.length, address: "", coordinates: x}]);
+    }
+    console.log(routeCoords.current);
+  }
+
+
+  /*function addNewAddress(e) {
+    e.preventDefault();
+    setAddresses([...addresses, input]);
+  }*/
+  // <button onClick={addNewAddress} type="submit" className="btn-primary">Добавить адрес</button>
+  return (
+    <div>
+      <div onChange={event => {setInputType(event.target.value)}}>
+        <div><input type="radio" value="search" defaultChecked  name="inputType"/> Поиск по адресу </div>
+        <div><input type="radio" value="coords" name="inputType"/> Ввод координат </div>
+      </div>
+      {inputType === "search" && <input value={input} onChange={onInputChange} size={70} type="text" id="suggest"/>}
+      {inputType === "coords" && 
+        <div>
+          <textarea value={input} onChange={onInputChange} style={{width: '532px'}} id="suggest"/>
+          <button onClick={setCoordsButtonOnClick} type="submit" className="btn-primary">Построить</button>
+          <span> или </span>
+          <button type="submit" className="btn-primary">Загрузить из файла</button>
+        </div>}
+      <div>
+        {coords.map(coord =>
+          <div>
+            <input value={coord.address} onChange={(e) => onCoordChange(e, coord)} size={70} type="text"/>
+            <span><button className="btn-danger" onClick={() => deleteCoord(coord)}>Удалить</button></span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ IITIK_HACK_map/ClientApp/src/components/Home.js
@@ -0,0 +1,130 @@
+import React, {useState, useEffect} from 'react';
+import {YMaps, Map, Placemark} from 'react-yandex-maps';
+import {AddressesView} from "./AddressesView";
+import {Row, Col} from "reactstrap";
+
+export const Home = () => {
+  const [coords, setCoord] = useState([]);
+  const [inputType, setInputType] = useState('search');
+
+  const map = React.useRef(null);
+  const ymaps = React.useRef(null);
+
+  let routeCoords = []; /*["Россия, Астрахань, Моздокская улица, 54 ", 
+    "Россия, Астрахань, Боевая улица, 62 ", 
+    "Россия, Астрахань, улица Татищева, 16 ", 
+    "Россия, Астраханская область, Приволжский район, а…орт Астрахань (Нариманово) имени Б.М. Кустодиева "];*/
+  const routeCoordsRef = React.useRef(routeCoords);
+  
+  
+
+  // обновляем маршрут, если координаты изменились
+  useEffect(() => {
+    if (map.current != null) {
+      buildRoute(ymaps.current);
+      //loadSuggest(ymaps.current);
+    }
+  }, [coords])
+
+  /* подсказки пока что ТОЛЬКО по адресу, 
+   * с yandex#search не получается получить координаты геокодером
+   * route тоже требует именно адрес */
+  const loadSuggest = ymaps => {
+    let inputElement = document.getElementById("suggest");
+    let lastValue = inputElement.value;
+    if (inputType === "search") {
+      let event = new Event('input', {bubbles: true});
+      const suggestView = new ymaps.SuggestView("suggest", {provider: 'yandex#map'});
+      suggestView.events.add("select", (e) => {
+        inputElement.value = e.get("item").value;
+        let tracker = inputElement._valueTracker;
+        if (tracker) {
+          tracker.setValue(lastValue);
+        }
+        inputElement.dispatchEvent(event);
+        // отключил, чтобы не потратить всё
+        // geocode(ymaps, e.get("item").value);
+        // вместо геокодера
+        setCoord(coords =>
+          [...coords, {index: coords.length, address: e.get("item").value, coordinates: ""}]);
+        routeCoords.push(e.get("item").value);
+      });
+    }
+  };
+
+  function buildRoute(ymaps) {
+    //map.current.geoObjects.removeAll();
+    console.log(routeCoordsRef.current);
+    /*const n = routeCoordsRef.current.length;
+    // матрица n*n заполненная нулями
+    let distanceMatrix = Array(n).fill(Array(n).fill(0));
+    
+    for (let i = 0; i < n; i++) {
+      for (let j = i + 1; j < n; j++) {
+        // получаем расстояние между coord[i] и coord[j]
+        ymaps.route([routeCoordsRef.current[i], routeCoordsRef.current[j]]).then((route) => {
+          distanceMatrix[i][j] = route.getLength();
+          console.log(i, j, routeCoordsRef.current[i], routeCoordsRef.current[j], route.getLength());
+        })
+      }
+    }
+    console.table(distanceMatrix);*/
+    map.current.geoObjects.removeAll();
+    ymaps.route(routeCoordsRef.current, {
+      mapStateAutoApply: true, reverseGeocoding: inputType === "coords"
+    }).then(function (route) {
+      route.getPaths().options.set({
+        strokeColor: '0000ffff',
+        opacity: 0.9
+      });
+      // добавляем маршрут на карту
+      map.current.geoObjects.add(route);
+      console.log(route.getLength());
+    });
+  }
+
+  const style = {
+    left: 0,
+    top: 0,
+    width: '73.82vw',
+    height: '90vh'
+  };
+  return (
+    <div>
+      <Row>
+
+        <Col xl="3">
+          <h1>Точки</h1>
+          <AddressesView coords={coords} setCoord={setCoord} routeCoords={routeCoordsRef} inputType={inputType} setInputType={setInputType}/>
+        </Col>
+
+        <Col>
+          <h1>Карта</h1>
+          <YMaps
+            query={{
+              ns: "use-load-option",
+              apikey: process.env.REACT_APP_YANDEX_MAP_API_KEY,
+              load: "geocode"
+            }}>
+            <div>
+              <Map style={style}
+                   instanceRef={map}
+                   onLoad={ymapsInstance => {
+                     loadSuggest(ymapsInstance);
+                     buildRoute(ymapsInstance);
+                     ymaps.current = ymapsInstance;
+                   }}
+                   modules={["SuggestView", "geocode", "route"]}
+                   defaultState={{center: [55.75, 37.57], zoom: 3}}>
+                {coords.map(coord =>
+                  <Placemark geometry={coord.coordinates}/>
+                )}
+              </Map>
+            </div>
+          </YMaps>
+        </Col>
+
+      </Row>
+    </div>
+  );
+}
